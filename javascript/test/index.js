839fd2443ddc784a19a790aad1aa5b0eb3844e59
whatthefoobar <irina.mntn@gmail.com>
2022-03-16 15:35:03 +0100

    test 1 done

--- javascript/test/index.js
+++ javascript/test/index.js
@@ -5,7 +5,7 @@ var { describe, it } = require("mocha");
 const Kata = require("../src/index");
 
 describe("Kata class", function () {
-  it("should return 1", function () {
+  it("should return 1!", function () {
     var kata = new Kata();
     var returnValue = kata.fizzBuzz(1);
 
